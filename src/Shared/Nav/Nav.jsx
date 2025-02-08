08f08725dba2b1f41afd5a876183b8e0761eade3
emonkhan500 <khdilo47@gmail.com>
2025-02-08 21:04:32 +0600

    working in nav

--- src/Shared/Nav/Nav.jsx
+++ src/Shared/Nav/Nav.jsx
@@ -97,9 +97,90 @@ const Nav = () => {
             </h1>
           </div>
         </div>
-
-        {/* mobile menu */}
         
+        {/* mobile menu */}
+        <aside
+          className={` ${
+            mobileSidebarOpen
+              ? "translate-x-0 opacity-100 z-20"
+              : "translate-x-[200px] opacity-0 z-[-1]"
+          } md:hidden bg-white boxShadow p-4 text-center absolute top-[55px] right-0 sm:w-[300px] w-full rounded-md transition-all duration-300`}
+        >
+          <ul className="items-start gap-[20px] text-[1rem] text-gray-600 flex flex-col">
+            <li className="hover:text-[#3B9DF8] group transition-all duration-500 cursor-pointer capitalize flex items-center gap-[10px]">
+              Home
+            </li>
+
+            <li
+              onClick={() => setMobileAboutUsOpen(!mobileAboutUsOpen)}
+              className="hover:text-[#3B9DF8] group transition-all duration-500 cursor-pointer capitalize flex items-center gap-[10px]"
+            >
+              About Us
+              <IoIosArrowDown
+                className={`${
+                  mobileAboutUsOpen ? "rotate-[180deg]" : "rotate-0"
+                } text-gray-600 group-hover:text-[#3B9DF8] transition-all duration-300`}
+              />
+            </li>
+
+            {/* about us mega menu */}
+            <div
+              className={`${
+                mobileAboutUsOpen ? "block" : "hidden"
+              } group font-[500] ml-6`}
+            >
+              <ul className="flex flex-col gap-[7px] text-[#424242]">
+                <li className="flex items-center gap-[7px] hover:text-[#3B9DF8] transition-all duration-300">
+                  <BsArrowRight className="text-[#424242] text-[0.9rem]" />{" "}
+                  Company Details
+                </li>
+              </ul>
+            </div>
+
+            <li
+              onClick={() => setMobileServiceOpen(!mobileServiceOpen)}
+              className="hover:text-[#3B9DF8] group transition-all duration-500 cursor-pointer capitalize flex items-center gap-[10px]"
+            >
+              Service
+              <IoIosArrowDown
+                className={`${
+                  mobileServiceOpen ? "rotate-0" : "rotate-[180deg]"
+                } text-gray-600 group-hover:text-[#3B9DF8] transition-all duration-300`}
+              />
+            </li>
+
+            {/* service mega menu */}
+            <div
+              className={`${
+                mobileServiceOpen ? "hidden" : "block"
+              } font-[500] ml-6`}
+            >
+              <ul className="flex flex-col gap-[7px] text-[#424242]">
+                <li className="flex items-center gap-[7px] hover:text-[#3B9DF8] transition-all duration-300">
+                  <BsArrowRight className="text-[#424242] text-[0.9rem]" />{" "}
+                  Company Details
+                </li>
+              </ul>
+
+              <div className="flex flex-col gap-[10px] mt-4">
+                <div className="flex items-center gap-[10px] text-[1rem] text-[#424242]">
+                  <MdDashboardCustomize className="bg-blue-200 text-blue-900 p-1.5 rounded-full text-[2rem]" />
+                  Full Customize
+                </div>
+
+                <div className="flex items-center gap-[10px] text-[1rem] text-[#424242]">
+                  <CgIfDesign className="bg-orange-200 text-orange-800 p-1.5 rounded-full text-[2rem]" />
+                  Modern Design
+                </div>
+
+                <div className="flex items-center gap-[10px] text-[1rem] text-[#424242]">
+                  <FaCubesStacked className="bg-yellow-200 text-yellow-800 p-1.5 rounded-full text-[2rem]" />
+                  Well Stacktured
+                </div>
+              </div>
+            </div>
+          </ul>
+        </aside>
       </nav>
       <SabNav></SabNav>
     </div>
