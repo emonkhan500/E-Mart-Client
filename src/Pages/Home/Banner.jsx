277ba11e51fccabf204e9fe3d6000be07c0122ea
emonkhan500 <khdilo47@gmail.com>
2025-02-15 13:44:16 +0600

    logo and name change in index

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import img1 from "../../assets/slider-1.png";
+import img2 from "../../assets/slider-2.png";
+
+const Banner = () => {
+  return (
+    
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
-        <div className='mt-96'>
-            <h1> This is HomePage</h1>
+        <div className='mt-20'>
+            <Banner></Banner>
         </div>
     );
 };
