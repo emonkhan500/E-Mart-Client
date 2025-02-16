20169e00fe4e3f587589d6b6986703cfd961bca0
emonkhan500 <khdilo47@gmail.com>
2025-02-16 14:31:35 +0600

    working on featured responsive

--- src/Pages/Home/Featured.jsx
+++ src/Pages/Home/Featured.jsx
@@ -1,50 +1,207 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
-import '../Home/Style.css';
+import "../Home/Style.css";
 
 // import required modules
-import { FreeMode, Pagination } from 'swiper/modules';
+import { FreeMode, Pagination } from "swiper/modules";
 
 const Featured = () => {
-    return (
-        <div className='mb-80'>
-            <div>
-                <h1 className='text-center mt-7  font-bold text-4xl'>Featured Categories</h1>
-            </div>
-            <Swiper
-        slidesPerView={7}
-        spaceBetween={30}
+  return (
+    <div className="mb-80 container">
+      <div className="w-full lg:w-[1586px]">
+        <h1 className="text-center mt-12 mb-6 font-bold text-4xl">
+          Featured Categories
+        </h1>
+      </div>
+      <Swiper
+        slidesPerView={10}
+        spaceBetween={1}
         freeMode={true}
         pagination={{
           clickable: true,
+        
         }}
         modules={[FreeMode, Pagination]}
-        className="mySwiper"
+        className="mySwiper w-full lg:w-[1586px] mx-auto"
+        breakpoints={{
+          320: { slidesPerView: 3, spaceBetween: 2 },   // Mobile (sm)
+          550: { slidesPerView: 4, spaceBetween: 2 },   // Small tablets
+          640: { slidesPerView: 5, spaceBetween: 3 },   // Tablets
+          770: { slidesPerView: 6, spaceBetween: 5 },   // Small laptops
+          1024: { slidesPerView: 10, spaceBetween:24 },  // Standard laptops (adjusted)
+          1280: { slidesPerView: 10, spaceBetween: 12 }, // Large laptops
+          1440: { slidesPerView: 11, spaceBetween: 15 }, // Widescreen monitors
+        }}
+        
       >
-        <SwiperSlide><div className='bg-[#F2FCE4] flex flex-col items-center rounded-lg pb-4 py-2'>
-                    <img className='' src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png" alt="" />
-                    <h1 className='text-base text-[#253D4E] font-semibold'>Cake & Milk</h1>
-                    <h1 className='text-[#7E7E7E]'>26 items</h1>
-                </div></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" lg:w-96  bg-[#F2FCE4] flex flex-col items-center rounded-lg py-2">
+            <img
+              className=""
+              src="https://i.ibb.co.com/QvGkSQvq/Link-cat-13-png.png"
+              alt=""
+            />
+            <h1 className="text-base text-[#253D4E] font-semibold px-4">
+              Cake & Milk
+            </h1>
+            <h1 className="text-[#7E7E7E]">26 items</h1>
+          </div>
+        </SwiperSlide>
+       
+        
       </Swiper>
-            
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Featured;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Featured from './Featured';
+import Footer from '../../Shared/Footer/Footer';
 
 const Home = () => {
     return (
         <div className=' lg:mt-10'>
             <Banner></Banner>
             <Featured></Featured>
+        <Footer></Footer>
         </div>
     );
 };
