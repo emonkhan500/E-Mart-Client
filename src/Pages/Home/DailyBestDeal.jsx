9439a328db02a620bd90d1935e67b5425acdf5fc
emonkhan500 <khdilo47@gmail.com>
2025-02-24 11:15:20 +0600

    working on daily best dealsy

---
+++ src/Pages/Home/DailyBestDeal.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DailyBestDeal = () => {
+    return (
+        <div>
+            <div className="md:my-7 my-4 mt-10 text-center">
+        <h1 className="text-[#253D4E] text-2xl md:text-4xl font-bold">
+        Daily Best Sells
+        </h1>
+      </div>
+            
+        </div>
+    );
+};
+
+export default DailyBestDeal;
--- src/Pages/Home/DealsOfDay.jsx
+++ src/Pages/Home/DealsOfDay.jsx
@@ -3,7 +3,7 @@ import { FaShoppingCart } from "react-icons/fa";
 
 const DealsOfDay = () => {
   return (
-    <div>
+    <div className="mt-16">
       <div className="md:my-7 my-4 mt-10 text-center">
         <h1 className="text-[#253D4E] text-2xl md:text-4xl font-bold">
           Deals Of The Day
