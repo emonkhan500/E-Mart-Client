046155ce79ab3ed8c65bd58082157e68705cd4d1
emonkhan500 <khdilo47@gmail.com>
2025-02-26 09:47:19 +0600

    almost design complete on welcome section

--- src/Pages/About/Welcome.jsx
+++ src/Pages/About/Welcome.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import '../About/Style.css';
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 const Welcome = () => {
     return (
-        <div>
-            <h1>Welcome to E-Mart</h1>
+        <div className='quick flex items-center flex-col gap-7 md:flex-row mt-10'>
+           <div className='rounded md::w-1/2'>
+            <img  src="https://i.ibb.co.com/ch9pJ2fX/Section-about-1-png.png" alt="" />
+           </div>
+           {/* content and slider */}
+           <div className='md:w-1/2'>
+           <div className='lato space-y-5'>
+            <h1 className='text-4xl font-bold text-[#253D4E]'>Welcome to <span className='  text-[#3BB77E]'>E-MART</span></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+reprehenderit in voluptate id est laborum.</p>
+
+
+           </div>
+           <div>
+           <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
+           </div>
+           </div>
         </div>
     );
 };
