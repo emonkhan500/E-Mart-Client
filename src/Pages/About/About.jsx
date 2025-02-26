6349d03da41144b26ce51e0e36d7479e011db332
emonkhan500 <khdilo47@gmail.com>
2025-02-27 00:05:35 +0600

    complete design on Glory section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Welcome from './Welcome';
 import Provide from './Provide';
 import Glory from './Glory';
+import Newsletter from '../Home/Newsletter';
+import Address from './Address';
+import Moto from './Moto';
 
 const About = () => {
     return (
         <div>
            <Welcome></Welcome>
            <Provide></Provide>
+           <Moto></Moto>
            <Glory></Glory>
+           <Address></Address>
+           <Newsletter></Newsletter>
         </div>
     );
 };
--- src/Pages/About/Glory.jsx
+++ src/Pages/About/Glory.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Glory = () => {
     return (
-        <div className="relative w-full mx-auto my-10 px-10">
+        <div className="quick relative w-full mx-auto my-10 px-5 md:px-10">
       {/* Background Image with Overlay */}
-      <div className="relative bg-cover bg-center h-48 flex items-center justify-center rounded-lg overflow-hidden" 
+      <div className="p-5 md:p-0 relative bg-cover bg-center md:h-56 flex items-center justify-center rounded-lg overflow-hidden" 
         style={{ backgroundImage: 'url(https://i.ibb.co.com/NcvGKjw/glory-bg.jpg)' }}>
         
         {/* Dark Overlay */}
@@ -12,12 +12,30 @@ const Glory = () => {
         
         {/* Stats Container */}
         <div className="relative z-10 flex justify-around w-full text-white text-center px-5">
-          {['Glorious years', 'Happy clients', 'Projects complete', 'Team advisor', 'Products Sale'].map((text, index) => (
-            <div key={index} className="flex flex-col items-center">
-              <span className="text-3xl font-bold">0+</span>
-              <span className="text-sm">{text}</span>
+          <div className='grid grid-cols-1 md:grid-cols-5 gap-10 lg:gap-36'>
+            <div className='flex flex-col'>
+            <span className=" text-3xl lg:text-4xl font-bold">2 +</span>
+              <span className="text-lg lg:text-xl font-semibold">Glorious years</span>
             </div>
-          ))}
+            <div className='flex flex-col'>
+            <span className=" text-3xl lg:text-4xl font-bold">10k +</span>
+              <span className="text-lg lg:text-xl font-semibold">Happy clients</span>
+            </div>
+            <div className='flex flex-col'>
+            <span className=" text-3xl lg:text-4xl font-bold">100 +</span>
+              <span className="text-lg lg:text-xl font-semibold">Projects complete</span>
+            </div>
+            <div className='flex flex-col'>
+            <span className=" text-3xl lg:text-4xl font-bold">50k +</span>
+              <span className="text-lg lg:text-xl font-semibold">Products Sale</span>
+            </div>
+            <div className='flex flex-col'>
+            <span className=" text-3xl lg:text-4xl font-bold">100 +</span>
+              <span className="text-lg lg:text-xl font-semibold">Team advisor</span>
+            </div>
+            
+            
+          </div>
         </div>
       </div>
     </div>
