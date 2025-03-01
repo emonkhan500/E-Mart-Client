e6da9d4e248e41d121d2a668168050f2e53f721e
emonkhan500 <khdilo47@gmail.com>
2025-03-01 19:35:41 +0600

    working on help page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Newsletter from '../Home/Newsletter';
+import Help from './Help';
 
 const Contact = () => {
     return (
-        <div>
-       
+       <div className='lato mt-16'>
+         <div>
+       <Help></Help>
             <Newsletter></Newsletter>
         </div>
+       </div>
     );
 };
 
--- src/Pages/Contact/Help.jsx
+++ src/Pages/Contact/Help.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Help = () => {
-    return (
-        <div>
-            <h1>This is help page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <div className="space-y-4 w-1/3">
+        <h1 className="text-[#3BB77E] text-lg font-bold quick">How can help you ?</h1>
+        <h1 className="text-3xl font-bold">Let us know how
+we can help you</h1>
+        <p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus
+leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus
+leo.</p>
+      </div>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Help;
