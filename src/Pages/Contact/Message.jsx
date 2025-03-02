9a7afe9e0c6a02914554e5616011bc64052f6214
emonkhan500 <khdilo47@gmail.com>
2025-03-02 19:37:26 +0600

    complete  message section

--- src/Pages/Contact/Message.jsx
+++ src/Pages/Contact/Message.jsx
@@ -10,43 +10,53 @@ const Message = () => {
     //   {/* contact form  */}
     //   <div></div>
     // </div>
+    <div className="border-t-2 mt-16">
+    <h1 className="text-4xl text-center font-bold mt-3 text-[#3BB77E]"> Contact Here ..</h1>
     <section className="w-full mb-8 lg:flex-row flex items-center gap-3 flex-col  lg:p-[40px] rounded-xl">
+       
       {/* form area */}
       <form className="lg:w-[60%] w-full">
         <div className="lg:w-[80%] w-full mx-auto">
-          
+          <div className="text-[#253D4E] text-center md:text-start">
+            <h1 className="text-xl md:text-3xl font-bold ">
+            Drop Us a Line
+            </h1>
+            <p className="text-[#B6B6B6] text-[0.9rem] mt-2 mb-2">
+            Your email address will not be published. Required fields are marked *
+            </p>
+          </div>
 
           <div className="flex sm:flex-row flex-col items-center gap-2 md:gap-[20px]">
             <div className="flex flex-col gap-[5px] w-full sm:w-[50%]">
               <input
                 type="text"
-                placeholder="Your name"
-                className="peer border-[#383844] border rounded-md outline-none px-4 py-2 lg:py-4 w-full  text-gray-400 transition-colors duration-300"
+                placeholder="Your Name"
+                className="peer border-[#383844] border rounded-md outline-none px-4 py-2 lg:py-4 w-full  text-black transition-colors duration-300"
               />
             </div>
 
             <div className="flex flex-col gap-[5px] w-full sm:w-[50%]">
               <input
                 type="email"
-                placeholder="Email address"
-                className="peer border-[#383844] border rounded-md outline-none px-4 py-2 lg:py-4 w-full text-gray-400 transition-colors duration-300"
+                placeholder="Your Email"
+                className="peer border-[#383844] border rounded-md outline-none px-4 py-2 lg:py-4 w-full text-black transition-colors duration-300"
               />
             </div>
           </div>
           <div className="flex mt-3 sm:flex-row flex-col items-center gap-2 md:gap-[20px]">
             <div className="flex flex-col gap-[5px] w-full sm:w-[50%]">
               <input
-                type="text"
-                placeholder="Your name"
-                className="peer border-[#383844] border rounded-md outline-none px-4 py-2 lg:py-4 w-full  text-gray-400 transition-colors duration-300"
+                type="number"
+                placeholder="Phone"
+                className="peer border-[#383844] border rounded-md outline-none px-4 py-2 lg:py-4 w-full  text-black transition-colors duration-300"
               />
             </div>
 
             <div className="flex flex-col gap-[5px] w-full sm:w-[50%]">
               <input
-                type="email"
-                placeholder="Email address"
-                className="peer border-[#383844] border rounded-md outline-none px-4 py-2 lg:py-4 w-full text-gray-400 transition-colors duration-300"
+                type="text"
+                placeholder="Subject"
+                className="peer border-[#383844] border rounded-md outline-none px-4 py-2 lg:py-4 w-full text-black transition-colors duration-300"
               />
             </div>
           </div>
@@ -54,7 +64,7 @@ const Message = () => {
           <div className="flex flex-col gap-[5px] w-full mt-[20px]">
             <textarea
               placeholder="Write message"
-              className="peer min-h-28 md:min-h-[200px] border-[#383844] border rounded-md outline-none px-4 py-3 w-full text-gray-400 transition-colors duration-300"
+              className="peer min-h-28 md:min-h-[200px] border-[#383844] border rounded-md outline-none px-4 py-3 w-full text-black transition-colors duration-300"
             ></textarea>
           </div>
 
@@ -76,6 +86,7 @@ const Message = () => {
         />
       </div>
     </section>
+    </div>
   );
 };
 
