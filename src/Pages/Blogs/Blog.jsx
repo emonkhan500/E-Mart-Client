a8f5fb9b5d4c5529426fd0f4c151e2674ec46123
emonkhan500 <khdilo47@gmail.com>
2025-03-04 11:37:29 +0600

    started working on blogcontent

--- src/Pages/Blogs/Blog.jsx
+++ src/Pages/Blogs/Blog.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BlogHeader from './BlogHeader';
+import BlogContent from './BlogContent';
 
 const Blog = () => {
     return (
         <div>
             <BlogHeader></BlogHeader>
+            <BlogContent></BlogContent>
         </div>
     );
 };
