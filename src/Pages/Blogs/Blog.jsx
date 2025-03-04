39a34d90106dba5c0745a3a13b4ae305091a9522
emonkhan500 <khdilo47@gmail.com>
2025-03-04 19:13:11 +0600

    complete  blogs design and

--- src/Pages/Blogs/Blog.jsx
+++ src/Pages/Blogs/Blog.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BlogHeader from './BlogHeader';
 import BlogContent from './BlogContent';
+import UpFooter from '../Home/UpFooter';
 
 const Blog = () => {
     return (
         <div>
             <BlogHeader></BlogHeader>
             <BlogContent></BlogContent>
+            <UpFooter></UpFooter>
         </div>
     );
 };
