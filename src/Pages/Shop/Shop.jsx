4c124921af66941e7d97aee792268a1c6cd8f13e
emonkhan500 <khdilo47@gmail.com>
2025-03-06 19:30:00 +0600

    2 shared component addedd

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ShareHead from '../../Shared/ShareHead';
+import DealsOfDay from '../Home/DealsOfDay';
+import SharedNewsletter from '../../Shared/SharedNewsletter';
 
 const Shop = () => {
     return (
         <div>
            <ShareHead pageTitle={'Shop'}></ShareHead>
+           <DealsOfDay></DealsOfDay>
+           <SharedNewsletter></SharedNewsletter>
+
         </div>
     );
 };
