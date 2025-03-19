eda8029bf2ac2a65e9b0650bad307f116b05f89e
emonkhan500 <khdilo47@gmail.com>
2025-03-19 11:11:20 +0600

    started working on google login

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from "../Authentication/Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const auth =getAuth(app)
+const googleProvider= new GoogleAuthProvider()
 const AuthProvider = ({children}) => {
   const [user,setUser]=useState(null)
   const [loading,setLoading]=useState(false)
@@ -16,12 +17,21 @@ return createUserWithEmailAndPassword(auth,email,pass)
 
 
 
+// googleLogin
+
+const googleLogin= ()=>{
+  setLoading(true)
+  return signInWithPopup(auth,googleProvider)
+}
+
+
 
 
   const authData = { 
     createUser,
     user,
-    loading
+    loading,
+    googleLogin,
   };
 
   return (
