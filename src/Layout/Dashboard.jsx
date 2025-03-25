43338dada1172baaeabca1c57ec9783445ac4e86
emonkhan500 <khdilo47@gmail.com>
2025-03-25 17:45:00 +0600

    heading imported

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from '../Shared/Nav/Nav';
 import Footer from '../Shared/Footer/Footer';
+import ShareHead from '../Shared/ShareHead';
 
 const Dashboard = () => {
     return (
         <div  className=' w-full max-w-[1600px] px-3 md:px-8 lg:px-12 xl:px-16 mx-auto mb-9'>
            <Nav></Nav>
+           <ShareHead pageTitle={'DASHBOARD'}></ShareHead>
            <Footer></Footer>
         </div>
     );
