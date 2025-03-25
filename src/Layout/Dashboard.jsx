1e08f3c483621a530b8dc0fc6e547a5e27b4bf4a
emonkhan500 <khdilo47@gmail.com>
2025-03-25 17:40:33 +0600

    nav imported

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Nav from '../Shared/Nav/Nav';
+import Footer from '../Shared/Footer/Footer';
 
 const Dashboard = () => {
     return (
-        <div>
-           <h1>This is dashboard</h1> 
+        <div  className=' w-full max-w-[1600px] px-3 md:px-8 lg:px-12 xl:px-16 mx-auto mb-9'>
+           <Nav></Nav>
+           <Footer></Footer>
         </div>
     );
 };
