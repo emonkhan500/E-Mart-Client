5bb281bf66d43d2fa95f5750b953320db0f13f15
emonkhan500 <khdilo47@gmail.com>
2025-03-25 17:50:33 +0600

    newsletter imported

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Nav from '../Shared/Nav/Nav';
 import Footer from '../Shared/Footer/Footer';
 import ShareHead from '../Shared/ShareHead';
+import Newsletter from '../Shared/SharedNewsletter';
 
 const Dashboard = () => {
     return (
         <div  className=' w-full max-w-[1600px] px-3 md:px-8 lg:px-12 xl:px-16 mx-auto mb-9'>
            <Nav></Nav>
            <ShareHead pageTitle={'DASHBOARD'}></ShareHead>
+           <Newsletter></Newsletter>
            <Footer></Footer>
         </div>
     );
