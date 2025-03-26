4d0aa7d7f48aa2ccd68e148326a74dfb77b1d62b
emonkhan500 <khdilo47@gmail.com>
2025-03-26 21:52:02 +0600

    started design on dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import Nav from '../Shared/Nav/Nav';
-import Footer from '../Shared/Footer/Footer';
-import ShareHead from '../Shared/ShareHead';
-import Newsletter from '../Shared/SharedNewsletter';
-import UpFooter from '../Pages/Home/UpFooter';
+import React from "react";
+import Nav from "../Shared/Nav/Nav";
+import Footer from "../Shared/Footer/Footer";
+import ShareHead from "../Shared/ShareHead";
+import Newsletter from "../Shared/SharedNewsletter";
+import UpFooter from "../Pages/Home/UpFooter";
 
 const Dashboard = () => {
-    return (
-        <div  className=' w-full max-w-[1600px] px-3 md:px-8 lg:px-12 xl:px-16 mx-auto mb-9'>
-           <Nav></Nav>
-           <ShareHead pageTitle={'DASHBOARD'}></ShareHead>
-           <Newsletter></Newsletter>
-           <UpFooter></UpFooter>
-           <Footer></Footer>
-        </div>
-    );
+  return (
+    <div className=" w-full max-w-[1600px] px-3 md:px-8 lg:px-12 xl:px-16 mx-auto mb-9">
+      <Nav></Nav>
+      <ShareHead pageTitle={"DASHBOARD"}></ShareHead>
+      {/* layout */}
+      <div>
+        <h1> started design dashboard</h1>
+      </div>
+      <Newsletter></Newsletter>
+      <UpFooter></UpFooter>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Dashboard;
