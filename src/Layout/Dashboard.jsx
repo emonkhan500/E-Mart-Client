aa901a70d9cef6f8e522aa336ed7b42703c264b7
emonkhan500 <khdilo47@gmail.com>
2025-04-03 22:02:11 +0600

    working on dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -5,10 +5,10 @@ import ShareHead from "../Shared/ShareHead";
 import Newsletter from "../Shared/SharedNewsletter";
 import UpFooter from "../Pages/Home/UpFooter";
 import { NavLink, Outlet } from "react-router-dom";
-import { IoMdHome } from "react-icons/io";
-import { FaUsers } from "react-icons/fa";
-import { IoMenu } from "react-icons/io5";
-import { HiOutlineHomeModern } from "react-icons/hi2";
+import { CgProfile } from "react-icons/cg";
+import { AiOutlineUnorderedList } from "react-icons/ai";
+import { IoIosLogOut } from "react-icons/io";
+
 
 
 
@@ -21,35 +21,42 @@ const Dashboard = () => {
     <div>
         <Nav></Nav>
    
-    <div className="flex flex-col md:flex-row  h-[100vh] px-2 md:px-10 lg:px-32 xl:px-44">
+    <div className="flex flex-col md:flex-row md:gap- lg:gap-36  h-[100vh] px-2 md:px-10 lg:px-32 xl:px-44">
     
     <div className=" w-auto md:w-64 lg:w-80 lg:min-h-screen  pt-20">
-    <ul className="menu space-y-2 mx-5">
-  <li>
+    <ul className="menu space-y-2 mx-5 ">
+  <li className="text-center">
     <NavLink 
       to='/dashboard/userprofile' 
       className={({ isActive }) => 
         `text-lg  font-bold border rounded-md px-3 py-3 transition ${
-          isActive ? 'bg-[#3BB77E] text-white' : 'bg-white border-gray-300'
+          isActive ? 'bg-[#3BB77E] text-white' : 'bg-white border-slate-300'
         } hover:bg-[#3BB77E] hover:text-white`
       }
     >
-      <IoMdHome /> Profile
+      <CgProfile /> Profile
     </NavLink>
   </li>
   
-  <li>
+  <li className="text-center">
     <NavLink 
       to='/dashboard/orders' 
       className={({ isActive }) => 
         `text-lg border font-bold rounded-md px-3 py-3 transition ${
-          isActive ? 'bg-[#3BB77E] text-white' : 'bg-white border-gray-300'
+          isActive ? 'bg-[#3BB77E] text-white' : 'bg-white border-slate-300'
         } hover:bg-[#3BB77E] hover:text-white`
       }
     >
-      <HiOutlineHomeModern /> Orders
+      <AiOutlineUnorderedList /> Orders
     </NavLink>
   </li>
+  <li className="text-center">
+  <button 
+    className="text-lg border font-bold rounded-md px-3 py-3 transition bg-white border-slate-300 hover:bg-[#3BB77E] hover:text-white"
+  >
+    <IoIosLogOut /> Logout
+  </button>
+</li>
 </ul>
 
     </div>
