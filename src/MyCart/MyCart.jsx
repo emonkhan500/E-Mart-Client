47372d7ce3dc269811a3e3d7406c3c1cfac8a0dd
emonkhan500 <khdilo47@gmail.com>
2025-04-12 21:35:02 +0600

    working on cart page

---
+++ src/MyCart/MyCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShareHead from '../Shared/ShareHead';
+
+const MyCart = () => {
+    return (
+        <div>
+           <ShareHead pageTitle={'My Cart'}></ShareHead>
+        </div>
+    );
+};
+
+export default MyCart;
