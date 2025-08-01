0d405ec5e834a192180abfc80a192a89b233e10a
emonkhan500 <khdilo47@gmail.com>
2025-08-01 23:45:09 +0600

    working on delete

--- src/AdminThing/ManageUser.jsx
+++ src/AdminThing/ManageUser.jsx
@@ -4,6 +4,7 @@ import { RiAdminLine } from "react-icons/ri";
 import { AuthContext } from "../Provider/AuthProvider";
 import useAxiosSecure from "../Axios/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const ManageUser = () => {
     const axiosSecure = useAxiosSecure()
@@ -14,13 +15,43 @@ const ManageUser = () => {
         return res.data
       }
     })
- console.log(users);
+//  console.log(users);
+//User delete
+const handleDelete=id=>{
+  console.log(id);
+  Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+          axiosSecure.delete(`/users/${id}`)
+          .then(res=>{
+              if(res.data.deletedCount>0){
+                  refetch()
+                  Swal.fire({
+                      title: "Deleted!",
+                      text: "Your file has been deleted.",
+                      icon: "success"
+                    });
+              }
+          })
+      
+      }
+    });
+}
+
   return (
     <div className="overflow-x-auto w-full  ">
       <table className="table border-2 border-slate-200 p-3">
         {/* head */}
         <thead className="text-2xl bg-[#3BB77E] text-white quick">
           <tr>
+            <th>No.</th>
             <th>Name</th>
             <th>Email</th>
             <th>Make Admin</th>
@@ -32,7 +63,8 @@ const ManageUser = () => {
         <tbody>
 
 {
-  users?.map(user=><tr className="border-b-2 border-slate-100">
+  users?.map((user,index) =><tr className="border-b-2 border-slate-100">
+  <td className="text-lg font-bold text-[#253D4E]">{index+1}</td>
   <td className="text-lg font-bold text-[#253D4E]">{user.name}</td>
   <td className="text-xl font-bold text-[#253D4E]">{user.email}</td>
   
@@ -42,7 +74,7 @@ const ManageUser = () => {
     </button>
   </th>
   <th>
-    <button className="btn btn-ghost btn-lg">
+    <button onClick={()=>handleDelete(user?._id)} className="btn btn-ghost btn-lg">
       <FaRegTrashAlt />
     </button>
   </th>
--- src/Axios/useAxiosSecure.jsx
+++ src/Axios/useAxiosSecure.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000',
 })
