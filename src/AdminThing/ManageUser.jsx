ff84de207ea9fd4dc2e7ab2f17f32f5a45819850
emonkhan500 <khdilo47@gmail.com>
2025-08-02 13:26:14 +0600

    working on admin

--- src/AdminThing/ManageUser.jsx
+++ src/AdminThing/ManageUser.jsx
@@ -16,6 +16,25 @@ const ManageUser = () => {
       }
     })
 //  console.log(users);
+
+// make admin
+const handleAdmin =(id)=>{
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, Make Admin!"
+  }).then((result) => {
+    
+  });
+  
+}
+
+
+
 //User delete
 const handleDelete=id=>{
   console.log(id);
