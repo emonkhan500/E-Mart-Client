30872bd3ed17ed28b50b4f05f233b5d2480e9331
emonkhan500 <khdilo47@gmail.com>
2025-10-01 11:21:41 +0600

    admin profile

--- src/AdminThing/AdminProfile.jsx
+++ src/AdminThing/AdminProfile.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const AdminProfile = () => {
-    return (
-        <div>
-            <h1 className='text-4xl font-bold mb-10'>Hello Rosie!</h1>
-            <p className='font-semibold text-lg'>From your account dashboard. you can easily check & view your recent orders,
-manage your shipping and billing addresses and edit your password and account details.</p>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  return (
+    <div className="px-5">
+      <h1 className="text-4xl font-bold mb-10 text-center">Hello <span className="text-[#3BB77E]">{user?.displayName}</span> !</h1>
+      <p className="font-medium text-lg">
+  From your account dashboard, you can quickly view recent orders, manage your shipping 
+  and billing information, and update your account details. <br /><br />
+  As an <span className="font-semibold text-[#3BB77E]">Admin</span>, you have full control over the system — 
+  you can <span className="font-semibold">add new products</span>, 
+  <span className="font-semibold"> manage users</span>, and 
+  <span className="font-semibold"> oversee product listings</span> to keep everything running smoothly.
+</p>
+    </div>
+  );
 };
 
 export default AdminProfile;
