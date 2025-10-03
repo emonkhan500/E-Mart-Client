33dec393d5c7f192bc9ed93b2fcc3e05fdc3bfdc
emonkhan500 <khdilo47@gmail.com>
2025-10-03 12:32:45 +0600

    workin on all product showingt

--- src/AdminThing/ManageProduct.jsx
+++ src/AdminThing/ManageProduct.jsx
@@ -23,6 +23,7 @@ console.log(allProduct);
         {/* head */}
         <thead className="text-2xl text-white bg-[#3BB77E] quick">
           <tr>
+            <th>#</th>
             <th>Product</th>
             <th>Name</th>
             <th>Price</th>
@@ -31,67 +32,42 @@ console.log(allProduct);
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          <tr className="border-b-2 border-slate-100">
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className=" h-24 w-24 border-2 border-slate-100 p-3">
-                    <img
-                      src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                      alt="Avatar Tailwind CSS Component"
-                    />
-                  </div>
+
+
+
+        {
+          allProduct?.map((product,index)=><tr className="border-b-2 border-slate-100">
+          <td>
+            <div className="flex items-center gap-3">
+              <div className="avatar">
+                <div className=" h-24 w-24 border-2 border-slate-100 p-3">
+                  <img
+                    src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                    alt="Avatar Tailwind CSS Component"
+                  />
                 </div>
               </div>
-            </td>
-            <td className="text-lg font-bold text-[#253D4E]">
-              Field Roast Chao Cheese Creamy Original
-            </td>
-            <td className="text-xl font-bold text-[#3BB77E]">$2.5</td>
-            {/* edit */}
-            <th>
-              <button className="btn btn-ghost btn-xl">
-              <CiEdit />
-              </button>
-            </th>
-            <th>
-              <button className="btn btn-ghost btn-lg">
-                <FaRegTrashAlt />
-              </button>
-            </th>
-          </tr>
-          {/* row 2 */}
-          <tr className="border-b-2 border-slate-100">
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className=" h-24 w-24 border-2 border-slate-100 p-3">
-                    <img
-                      src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                      alt="Avatar Tailwind CSS Component"
-                    />
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td className="text-lg font-bold text-[#253D4E]">
-              Field Roast Chao Cheese Creamy Original
-            </td>
-            <td className="text-xl font-bold text-[#3BB77E]">$2.5</td>
-            {/* edit */}
-            <th>
-              <button className="btn btn-ghost btn-xl">
-              <CiEdit />
-              </button>
-            </th>
-            {/* delete */}
-            <th>
-              <button className="btn btn-ghost btn-lg">
-                <FaRegTrashAlt />
-              </button>
-            </th>
-          </tr>
+            </div>
+          </td>
+          <td className="text-lg font-bold text-[#253D4E]">
+            Field Roast Chao Cheese Creamy Original
+          </td>
+          <td className="text-xl font-bold text-[#3BB77E]">$2.5</td>
+          {/* edit */}
+          <th>
+            <button className="btn btn-ghost btn-xl">
+            <CiEdit />
+            </button>
+          </th>
+          <th>
+            <button className="btn btn-ghost btn-lg">
+              <FaRegTrashAlt />
+            </button>
+          </th>
+        </tr>)
+        }
+          
+          
         </tbody>
       </table>
     </div>
