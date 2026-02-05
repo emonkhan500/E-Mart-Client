15a84b9f7bb5c40172ee41196d6634add66cde30
emonkhan500 <khdilo47@gmail.com>
2026-02-05 15:39:09 +0600

    footer complete

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -10,41 +10,50 @@ import logo from "../../assets/logo.svg";
 const Footer = () => {
   return (
     <div className=" mt-10 md:mt-12 lg:mt-14  quick px-4 pt-2 md:border shadow md:shadow-md rounded ">
-      <div className="grid pt-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-6 text-primary-text gap-10">
+      <div className="grid pt-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 text-primary-text gap-6 md:gap-10">
         {/* clo-1 */}
-        <div className="space-y-4">
-          <div className="mb-8">
+        <div className="space-y-2 md:space-y-4">
+          <div className="mb-6">
             <img src={logo} alt="" />
           </div>
           <h1 className="text-base md:text-lg">
-            <span className="text-lg md:text-xl">Buy Fresh - Eat Healthy!</span>{" "}
+            <span className="text-base md:text-xl">
+              Buy Fresh - Eat Healthy!
+            </span>{" "}
             <br />
             Buy Your grocery from us.{" "}
           </h1>
-          <h1 className="flex flex-wrap  gap-2 items-center ">
-            <CiLocationOn className="flex text-lg text-primary-green font-bold" />{" "}
-            <span className="text-lg text-primary-green">Address:</span>
+          <h1 className="flex flex-wrap  items-center ">
+            <CiLocationOn className="flex text-sm md:text-lg text-primary-green font-bold" />{" "}
+            <span className="text-sm md:text-lg text-primary-green">
+              Address:
+            </span>
             Dhaka,Bangladesh
           </h1>
-          <h1 className="flex flex-wrap gap-2 items-center ">
+          <h1 className="flex flex-wrap items-center ">
             <MdCall className="flex font-bold items-center text-primary-green" />{" "}
-            <span className="text-lg text-primary-green">Call Us:</span>
+            <span className="text-sm md:text-lg text-primary-green">
+              Call Us:
+            </span>
             (+880)-1884932158
           </h1>
-          <h1 className="flex flex-wrap gap-2 items-center ">
+          <h1 className="flex flex-wrap items-center ">
             <CiMail className="flex font-bold items-center text-primary-green text-base" />
             <span className="text-xs text-primary-green">Email:</span>
             emon562002@gamil.com
           </h1>
-          <h1 className="flex gap-2 items-center ">
+          <h1 className="flex items-center ">
             <IoMdTime className="flex items-center font-bold text-primary-green" />{" "}
-            <span className="text-lg text-primary-green">Hours:</span> 24/7
+            <span className="text-sm md:text-lg text-primary-green">
+              Hours:
+            </span>{" "}
+            24/7
           </h1>
         </div>
         {/* clo-2 */}
         <div>
-          <h1 className=" text-primary-gray">Company</h1>
-          <div className="space-y-2 md:space-y-5 text-lg">
+          <h1 className=" text-primary-gray mb-2">Company</h1>
+          <div className="space-y-2 md:space-y-5 text-sm md:text-lg">
             <h1>About Us</h1>
             <h1>Delivery Information</h1>
             <h1>Privacy Policy</h1>
@@ -55,8 +64,8 @@ const Footer = () => {
         </div>
         {/* clo-3 */}
         <div>
-          <h1 className="mb-3 text-3xl font-bold">Account</h1>
-          <div className="space-y-2 md:space-y-5 text-lg">
+          <h1 className="mb-3 text-xl md:text-3xl font-bold">Account</h1>
+          <div className="space-y-2 md:space-y-5 text-sm md:text-lg">
             <h1>View Cart</h1>
             <h1>My Wishlist</h1>
             <h1>Track My Order</h1>
@@ -67,8 +76,8 @@ const Footer = () => {
         </div>
         {/* clo-4 */}
         <div>
-          <h1 className="mb-3 text-3xl font-bold">Corporate</h1>
-          <div className="space-y-2 md:space-y-5 text-lg">
+          <h1 className="mb-3 text-xl md:text-3xl font-bold">Corporate</h1>
+          <div className="space-y-2 md:space-y-5 text-sm md:text-lg">
             <h1>Become a Vendor</h1>
             <h1>Affiliate Program</h1>
             <h1>Farm Business</h1>
@@ -79,8 +88,8 @@ const Footer = () => {
         </div>
         {/* clo-5 */}
         <div>
-          <h1 className="mb-3 text-3xl font-bold">Popular</h1>
-          <div className="space-y-2 md:space-y-5 text-lg">
+          <h1 className="mb-3 text-xl md:text-3xl font-bold">Popular</h1>
+          <div className="space-y-2 md:space-y-5 text-sm md:text-lg">
             <h1>Milk & Flavoured Milk</h1>
             <h1>Butter and Margarine</h1>
             <h1>Eggs Substitutes</h1>
@@ -91,8 +100,8 @@ const Footer = () => {
         </div>
         {/* clo-6 */}
         <div>
-          <h1 className="mb-6 text-3xl font-bold">Install App</h1>
-          <h1 className=" font-semibold mt-20">
+          <h1 className="mb-6 text-xl md:text-3xl font-bold">Install App</h1>
+          <h1 className=" font-semibold md:mt-10 lg::mt-20">
             From App Store or Google Play
           </h1>
           <div className="flex flex-col lg:w-3/6  shadow-md rounded lg:flex-row gap-2 mt-4">
@@ -103,33 +112,38 @@ const Footer = () => {
       </div>
 
       {/* subFooter */}
-      <div className="flex flex-col md:flex-row justify-between border-t-primary-border border-t-2 mt-16 mb-8 px-4 md:px-0 py-6 gap-6 md:gap-0 text-center md:text-left">
+      <div className="flex flex-col md:flex-row justify-between border-t-primary-border border-t-2 mt-10 md:mt-16 mb-8 px-4 md:px-0 py-6 gap-6 md:gap-0 text-center md:text-left">
         <div className="font-semibold">
           <h1>
-            © 2025,{" "}
-            <span className="text-lg text-primary-green">E-Mart & Grocery</span>
+            <span className="text-base md:text-lg text-primary-green">
+              E-Mart & Grocery
+            </span>
           </h1>
-          <h1 className="text-primary-gray">All rights reserved</h1>
+          <h1 className="text-primary-gray text-[12px] md:text-base">
+            {new Date().getFullYear()} All rights reserved
+          </h1>
         </div>
         <div className="flex flex-col md:flex-row items-center gap-4 md:gap-1 lg:gap-4 font-semibold">
-          <BiSupport className="text-primary-text text-4xl" />
+          <BiSupport className="text-primary-text text-2xl md:text-4xl" />
           <div>
-            <h1 className="text-primary-green text-lg font-bold">
+            <h1 className="text-primary-green text-base md:text-lg font-bold">
               +8801884932158
             </h1>
-            <h1 className="text-primary-gray text-sm">24/7 Support Center</h1>
+            <h1 className="text-primary-gray text-[12px] md:text-base">
+              24/7 Support Center
+            </h1>
           </div>
         </div>
         <div className="font-semibold">
           <div className="flex items-center justify-center gap-3 md:gap-1 lg:gap-3">
-            <h1 className="text-lg font-bold">Follow Us :</h1>
+            <h1 className="text-sm md:text-lg font-bold">Follow Us :</h1>
             <div className="flex justify-center    space-x-2 ">
               {/* Icon 1  */}
               <a
                 href="https://github.com/emonkhan500"
                 className=" rounded-lg p-2 "
               >
-                <FaGithub className="w-6 h-6 text-primary-green"></FaGithub>
+                <FaGithub className="w-4 md:w-6 h-6 text-primary-green"></FaGithub>
               </a>
 
               {/* Icon 2  */}
@@ -137,7 +151,7 @@ const Footer = () => {
                 href="https://www.linkedin.com/in/md-emon-khan"
                 className=" rounded-lg p-2 "
               >
-                <FaLinkedin className="w-6 h-6 text-primary-green"></FaLinkedin>
+                <FaLinkedin className="w-4 md:w-6 h-6 text-primary-green"></FaLinkedin>
               </a>
 
               {/* Icon 3  */}
@@ -145,7 +159,7 @@ const Footer = () => {
                 href="https://www.facebook.com/profile.php?id=100025282659899&mibextid=ZbWKwL"
                 className=" rounded-lg p-2 "
               >
-                <FaFacebookF className="w-6 h-6 text-primary-green" />
+                <FaFacebookF className="w-4 md:w-6 h-6 text-primary-green" />
               </a>
 
               {/* Icon 4 */}
@@ -153,11 +167,11 @@ const Footer = () => {
                 href="https://www.instagram.com/em0_n/"
                 className=" rounded-lg p-2 "
               >
-                <FaInstagram className="w-6 h-6 text-primary-green" />
+                <FaInstagram className="w-4 md:w-6 h-6 text-primary-green" />
               </a>
             </div>
           </div>
-          <h1 className="text-primary-gray">
+          <h1 className="text-primary-gray text-[12px] md:text-base">
             Up to 15% discount on your first subscribe
           </h1>
         </div>
